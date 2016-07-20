c42d2a50ac8b748c81c33859e3c67ba63a0262d9
Aaron Jaeger <Aaron Jaeger>
2016-07-20 19:28:21 -0400

    slave sends buffered pulses at correct times. I have been unable to trigger failure caused by timing, found 1 small edge case where touch can be scored by leaving blade on opponent and then removing blade quickly, fixing this would require faster accelerometer that could be read prior to saber reading.

--- slave_cord/slave_cord.ino
+++ slave_cord/slave_cord.ino
@@ -24,7 +24,7 @@ MMA8452Q saberAccel ;
 #define D1 67
 #define D2 73
 #define D3 80
-int timeState =-1; 
+int timeState = -1;
 #define SETUP 1
 #define READ_WRITE 2
 #define END_READ 3
@@ -37,12 +37,12 @@ const int lOut = 6;
 const int sOut = 5;
 const int saberRead = 3; // can only be 2 or 3 for interrupt;
 const int usDelay = 10;
-const float accelThreshold = 1.7; 
+const float accelThreshold = 1.7;
 volatile int timeStep = 0;
 volatile bool synchronized = false;
 volatile int wire_State;
 int data;
-int opState =0; 
+int opState = 0;
 void setup() {
   Timer1.initialize(400); //(2.5khz);
   Timer1.attachInterrupt(timerISR);
@@ -62,15 +62,15 @@ void setup() {
 
   saberAccel.init();
   Serial.begin(115200);
-  Serial.println("Slave V0.6 Started.");
+  Serial.println("Slave V2 Started.");
 
 }
 
 volatile bool ignoreInterrupt = false;
 volatile bool ignoreSaber = false;
 volatile bool sendPulse = false;
-bool pulseBuffer = false; 
-bool goodConnection = false; 
+bool pulseBuffer = false;
+bool goodConnection = false;
 bool hasBeenWritten = false;
 int timeSinceRestart = 0;
 
@@ -79,7 +79,7 @@ void readISR() {
     synchronized = true;
     timeStep = 0;
     timeSinceRestart = 0;
-    ; 
+    ;
   }
 }
 
@@ -114,177 +114,162 @@ void loop() {
     ////////////
     // handle pulse buffer for scoring touch
     ///////////
-    
-    if(pulseBuffer && t >= D1 && t <= D2){
-      sendPulse = true; 
-       
-      Serial.println("pulseBuffer used"); 
-    } 
-    else{  
+
+
     ///////////////////////////
     //read saber routine
     ///////////////////////////////
     if (!ignoreSaber) {
-      for(int i = 0; i < 2; i++){ 
+      for (int i = 0; i < 2; i++) {
         int saberVal = digitalRead(saberRead);
-        if(saberVal == 0){ 
-          goodConnection = false; 
-          break; 
+        if (saberVal == 0) {
+          goodConnection = false;
+          break;
         }
-        else{ 
-          goodConnection = true;  
-        } 
+        else {
+          goodConnection = true;
+        }
       }
-    } 
+    }
 
-    
-   ///////////
-   // check timing of hit, & then check accelerometer.  
-   ////////////
-   
-    if(goodConnection){ 
-      goodConnection = false; 
-       ///////////
-       // if hit happens during "touch" period check accelerometer
-       // simple version, just checks all three axis to see if they are above threshold. 
-       // if above threshold, pulsBuffer is true; 
-       if (timeStep >= D1 && timeStep <= D2) {
-        Serial.println("timing correct"); 
-          if (saberAccel.available()) {
-            saberAccel.read();
-            if (accelThreshold < abs(saberAccel.cx) 
-            || accelThreshold < abs(saberAccel.cy) 
-            || accelThreshold < abs(saberAccel.cz) ) {
-              pulseBuffer = true; 
-              Serial.println("passed threshold"); 
-            }
-            else{ 
-              pulseBuffer = false; 
-            }
+
+    ///////////
+    // check timing of hit, & then check accelerometer.
+    ////////////
+
+    if (goodConnection) {
+      goodConnection = false;
+      ///////////
+      // if hit happens during "touch" period check accelerometer
+      // simple version, just checks all three axis to see if they are above threshold.
+      // if above threshold, pulsBuffer is true;
+      if (timeStep >= D1 && timeStep <= D2) {
+        // Serial.print("t ");
+        //Serial.println(timeStep);
+
+        if (saberAccel.available()) {
+          saberAccel.read();
+          if (accelThreshold < abs(saberAccel.cx)
+              || accelThreshold < abs(saberAccel.cy)
+              || accelThreshold < abs(saberAccel.cz) ) {
+            pulseBuffer = true;
+            //Serial.println("passed threshold");
+          }
+          else {
+            pulseBuffer = false;
           }
         }
-      else{ 
-        
-      Serial.print("bad Sync "); 
-      Serial.println(timeStep);
+      }
+      else {
+
+        Serial.print("bad Sync ");
+        Serial.println(timeStep);
         sendPulse = false; //  true;
       }
-     }
-    } 
-    
-    
+    }
+
+
+
     ////////////////////////////
-    // Send pulse routine 
+    // Send pulse routine
     /////////////////////////////
-    if (!ignoreSaber && sendPulse) {
-      Serial.println("sending");
-        //noInterrupts();
-        ignoreInterrupt = true;
-        ignoreSaber = true;
-        digitalWrite(writePin, HIGH);
-        sendPulse = false;
-        pulseBuffer = false;
-        delayMicroseconds(usDelay);
-        digitalWrite(writePin, LOW);
-        //delay(9);
-        
-        ignoreInterrupt = false;
+    if (!ignoreSaber && sendPulse || (pulseBuffer && timeStep >= D1 && timeStep <= D2)) {
+      int ts = timeStep;
+      //noInterrupts();
+      ignoreInterrupt = true;
+      ignoreSaber = true;
+      digitalWrite(writePin, HIGH);
+      sendPulse = false;
+      pulseBuffer = false;
+      delayMicroseconds(usDelay);
+      digitalWrite(writePin, LOW);
+      //delay(9);
 
+      ignoreInterrupt = false;
 
-        // Serial.println(timeStep);
-      }
+     //Serial.print("s ");
+     // Serial.println(ts);
+    }
 
     //////////////////////////////////
     // Set pins routine
-    ////////////////////////////////// 
-//1 // timeStep: 0 -> A0: setup S High, L Low, disable interrupt, no writing to master
+    //////////////////////////////////
+    //1 // timeStep: 0 -> A0: setup S High, L Low, disable interrupt, no writing to master
 
-//2 // timeStep 20 -> B0: setup S low, B High,
-// timeStep 27 -> B1: enable interrupt, if high, send to master, (-> then disable interrupt so that only read once?)
-// timeStep 33 -> B2: disable interrupts
-//3 // timeStep 40 -> C0: setup S Low, B low
-// timeStep 47 -> C1: enable interrupts, if High, send to master
-// timeStep 53 -> C2: disable interrupts.
-//4 // timeStep 60 -> D0: setup S Low, B low (probably unecessary)
-// timeStep 67 -> D1: enable interrupts, if High, send to master
-// timeStep 73 -> D2: disable interrupts.
-// timeStep 80 -> D3: timeStep = -1;
-    
+    //2 // timeStep 20 -> B0: setup S low, B High,
+    // timeStep 27 -> B1: enable interrupt, if high, send to master, (-> then disable interrupt so that only read once?)
+    // timeStep 33 -> B2: disable interrupts
+    //3 // timeStep 40 -> C0: setup S Low, B low
+    // timeStep 47 -> C1: enable interrupts, if High, send to master
+    // timeStep 53 -> C2: disable interrupts.
+    //4 // timeStep 60 -> D0: setup S Low, B low (probably unecessary)
+    // timeStep 67 -> D1: enable interrupts, if High, send to master
+    // timeStep 73 -> D2: disable interrupts.
+    // timeStep 80 -> D3: timeStep = -1;
+
     // Saber set HIGH, Lame Low
     // Saber does not read.
-    if(timeStep >= A0 && timeStep < B0 && A0 != timeState){ 
+    if (timeStep >= A0 && timeStep < B0 && A0 != timeState) {
       ignoreSaber = true;
       digitalWrite(sOut, HIGH);
       digitalWrite(lOut, LOW);
-      timeState = A0; 
-    } 
+      timeState = A0;
+    }
 
-      //saber out Set LOW, Lame set HIGH
-    else if(timeStep >= B0 && timeStep < B1 && B0 != timeState ){ 
+    //saber out Set LOW, Lame set HIGH
+    else if (timeStep >= B0 && timeStep < B1 && B0 != timeState ) {
       digitalWrite(sOut, LOW);
       digitalWrite(lOut, HIGH);
-      timeState = B0; 
+      timeState = B0;
     }
-      // Allow saber read Interrrupt action
-      // short period to
-    else if(timeStep >= B1 && timeStep < B2 && B1 != timeState){ 
+    // Allow saber read Interrrupt action
+    // short period to
+    else if (timeStep >= B1 && timeStep < B2 && B1 != timeState) {
       // Serial.println("in self touch");
       ignoreSaber = false;
-      timeState = B1; 
-    } 
-    else if(timeStep >= B2 && timeStep < C0 && B2 != timeState){ 
-        ignoreSaber = true;
-        timeState = B2;
-    } 
-    else if(timeStep >= C0 && timeStep < C1 && C0 != timeState ){ 
-        digitalWrite(sOut, LOW);
-        digitalWrite(lOut, LOW);
-        timeState = C0;
-    } 
-    else if(timeStep >= C1 && timeStep < C2 && C1 != timeState){ 
-        ignoreInterrupt = true;
-        ignoreSaber = true;
-        digitalWrite(writePin, HIGH);
-        sendPulse = false;
-        pulseBuffer = false;
-        delayMicroseconds(usDelay);
-        digitalWrite(writePin, LOW);
-        //delay(9);
-        
-        ignoreInterrupt = false;
-        Serial.println("c1 sent"); 
-        ignoreSaber = false;
+      timeState = B1;
+    }
+    else if (timeStep >= B2 && timeStep < C0 && B2 != timeState) {
+      ignoreSaber = true;
+      timeState = B2;
+    }
+    else if (timeStep >= C0 && timeStep < C1 && C0 != timeState ) {
+      digitalWrite(sOut, LOW);
+      digitalWrite(lOut, LOW);
+      timeState = C0;
+    }
+    else if (timeStep >= C1 && timeStep < C2 && C1 != timeState) {
+      ignoreSaber = false;
+      timeState = C1;
 
-        timeState = C1; 
-        
-      
-    } 
-      else if(timeStep >= C2 && timeStep < D0 && C2 != timeState){
-        ignoreSaber = true;
-        timeState = C2;
-      } 
 
-      //    D0 operation unecessary because pin state in C0 and D0 is the same.
-      //    case D0:
-      //      digitalWrite(sOut, LOW);
-      //      digitalWrite(lOut, LOW);
-      //      break;
+    }
+    else if (timeStep >= C2 && timeStep < D0 && C2 != timeState) {
+      ignoreSaber = true;
+      timeState = C2;
+    }
 
-      else if(timeStep >= D1 && timeStep < D2 && D1 != timeState){
-        ignoreSaber = false;
-        timeState = D1;
-      } 
+    //    D0 operation unecessary because pin state in C0 and D0 is the same.
+    //    case D0:
+    //      digitalWrite(sOut, LOW);
+    //      digitalWrite(lOut, LOW);
+    //      break;
 
-     else if(timeStep >= D2 && timeStep < D3 && D2 != timeState){
-        ignoreSaber = true;
-        timeState = D2; 
-     } 
+    else if (timeStep >= D1 && timeStep < D2 && D1 != timeState) {
+      ignoreSaber = false;
+      timeState = D1;
+    }
 
-      else if(timeStep >= D3){
-        timeStep = -1;
-        
-      } 
-     
+    else if (timeStep >= D2 && timeStep < D3 && D2 != timeState) {
+      ignoreSaber = true;
+      timeState = D2;
+    }
+
+    else if (timeStep >= D3) {
+      timeStep = -1;
+
+    }
+
   } else {
     //unsynchronized
     Serial.print(".");
